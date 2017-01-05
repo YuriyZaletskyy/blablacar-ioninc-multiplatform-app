2575ef0879b5adf4ba5efea8eee5e75e46ec3822
Yuriy Zaletskyy <zaletskiy@gmail.com>
2017-01-05 21:05:17 +0200

    Implemented initial version of get request

---
+++ src/pages/config.ts
@@ -0,0 +1,33 @@
+﻿
+
+import { Component } from '@angular/core'
+
+@Component({
+})
+
+export class Config {
+    public apiKey: string = '7aa9393a47c24d8b9085187163f5d241';
+    public url: string = 'https://public-api.blablacar.com/api/v2/trips?key=';
+    //https://public-api.blablacar.com/api/v2/trips?key=7aa9393a47c24d8b9085187163f5d241&
+    constructor() {
+    }
+
+    getSearchUrl(fn: string, tn: string, locale: string = "en_US", format: string = "json",
+        cur: string = "USD", radius: string = "10", sort: string = "trip_price",
+        seats: Number = 2, limit: number = 50, dbFrom: string, dbTo: string): string {
+        let url: string = "http://localhost:8100/api/v2/trips?key=7aa9393a47c24d8b9085187163f5d241&";
+        debugger;
+        url = url + 'fn=' + fn;
+        url = url + '&' + 'tn=' + tn;
+        url = url + '&' + 'locale=' + locale;
+        url = url + '&_format=' + format;
+        url = url + '&cur=' + cur;
+        url = url + '&db=' + dbFrom.substring(0, 10);
+        url = url + '&de=' + dbTo.substring(0, 10);
+        url = url + '&hb=0&he=23';
+        url = url + '&page=1';
+        url = url + '&seats=' + seats;
+        url = url + '&sort=trip_price&order=desc&limit=50&radius=10';
+        return url;
+    }
+}
--- src/pages/findTrip/findTrip.ts
+++ src/pages/findTrip/findTrip.ts
@@ -1,9 +1,72 @@
-﻿import { Component } from '@angular/core'
+﻿import { Component, Injectable } from '@angular/core'
+import { Http, Response } from '@angular/http'
+import { FormsModule } from '@angular/forms'
+import { Config } from '../config'
+
+import 'rxjs';
+import { Observable } from 'rxjs/Observable'
 
 @Component({
     templateUrl: 'findTrip.html'
 })
 
 export class FindTrip {
-    constructor() { }
+
+    public findTripInit: FindTripInit = new FindTripInit();
+
+    public foundTrips: any = {};
+
+    constructor(private http: Http) {
+       
+    }
+
+    findTrips()
+    {
+        let conf = new Config();
+        let head = new headers();
+
+
+        let searchUrl: string = conf.getSearchUrl(this.findTripInit.from, this.findTripInit.to, "en_US", "json", "USD", "10", "asc", this.findTripInit.numberOfSeats, 50,
+            this.findTripInit.dateFrom, this.findTripInit.dateTo);
+
+        this.http.get(searchUrl, head)
+            .subscribe(
+            (r: any) => {//correct response
+                debugger;
+            },
+            (e: any) => {//error
+                debugger;
+            },
+            () => {//complete
+                debugger;
+            });
+
+        debugger;
+
+    }
+
+}
+
+export class headers
+{
+    public accept: string = "application/json";
+    public key: string = (new Config()).apiKey;
+}
+
+export class FindTripInit
+{
+    public from: string = "";
+    public to: string = "";
+    public dateFrom: string;
+    public dateTo: string;
+    public numberOfSeats: Number = 2;
+
+    constructor()
+    {
+        let currentDate: Date = new Date();
+        debugger;
+        this.dateFrom = currentDate.toISOString();
+        this.dateTo = "" + currentDate.toISOString();
+    }
+
 }
